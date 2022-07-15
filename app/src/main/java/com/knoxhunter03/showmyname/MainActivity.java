65c9bee02a1b9ba23672765fc5f83716648db8a8
knoxhunter03 <mangalamkallo03@gmail.com>
2022-07-16 03:46:56 +0530

    MODIFY Button To Show Text

--- app/src/main/java/com/knoxhunter03/showmyname/MainActivity.java
+++ app/src/main/java/com/knoxhunter03/showmyname/MainActivity.java
@@ -3,12 +3,30 @@ package com.knoxhunter03.showmyname;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button showButton; // Fetch the button first
+    private TextView myName; // Fetch the text View also
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showButton = findViewById(R.id.button); // Locate the button from R.id.
+        myName = findViewById(R.id.textView); // Locate the text View from R.id
+
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myName.setText("KnoxHunter");
+            }
+        });
+
+
+
     }
 }
