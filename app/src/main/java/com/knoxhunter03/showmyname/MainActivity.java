9b9c5164335cacdc942fb599fe0506cf5df27a04
knoxhunter03 <mangalamkallo03@gmail.com>
2022-07-16 23:58:14 +0530

    ADD EditText & Name Strings

--- app/src/main/java/com/knoxhunter03/showmyname/MainActivity.java
+++ app/src/main/java/com/knoxhunter03/showmyname/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button showButton; // Fetch the button first
     private TextView myName; // Fetch the text View also
+    private EditText showMyName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +20,22 @@ public class MainActivity extends AppCompatActivity {
 
         showButton = findViewById(R.id.button); // Locate the button from R.id.
         myName = findViewById(R.id.textView); // Locate the text View from R.id
+        showMyName = findViewById(R.id.editTextName);
 
         showButton.setOnClickListener(new View.OnClickListener() {
-            @Override
+
+           @Override
             public void onClick(View view) {
-                myName.setText("KnoxHunter");
+
+                String name = showMyName.getText().toString(); // Store the entered text into a string variable &
+                // Make sure it's converted to String so that everything is in string
+               if(name.isEmpty()){
+                   myName.setText("Hello Boss!");
+               }
+               else {
+                   myName.setText("Hello " + name);
+               }
+
             }
         });
 
