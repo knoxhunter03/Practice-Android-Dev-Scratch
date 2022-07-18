98658a720ba9afca89b40f82172a77a0fad64352
knoxhunter03 <mangalamkallo03@gmail.com>
2022-07-19 02:24:43 +0530

    ADD color change feature to TextView
    moneyValue -> TextView

--- MakeItRain/app/src/main/java/com/knoxhunter03/makeitrain/MainActivity.java
+++ MakeItRain/app/src/main/java/com/knoxhunter03/makeitrain/MainActivity.java
@@ -1,7 +1,9 @@
 package com.knoxhunter03.makeitrain;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
 import android.icu.text.NumberFormat;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private Button makeItRain;
     private Button showInfo;
     private TextView moneyValue;
+    private TextView congratsText;
 
     int moneyCounter = 0;
 
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         makeItRain = findViewById(R.id.buttonMakeItRain);
         showInfo = findViewById(R.id.buttonShowInfo);
         moneyValue = findViewById(R.id.moneyValue);
+        congratsText = findViewById(R.id.textView3);
         // makeItRain.setOnClickListener(view -> Log.d("Main_Activity", "onClick: Make It Rain!"));
 
 
@@ -35,7 +39,27 @@ public class MainActivity extends AppCompatActivity {
     public void showMoney(View view) {
         NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
         moneyCounter += 1000;
+        switch (moneyCounter){
+
+            case 20000:
+            moneyValue.setTextColor(ContextCompat.getColor(MainActivity.this,
+                    R.color.white));
+            break;
+            // congratsText.setText("Congratulations!");
+
+            case 30000:
+            moneyValue.setTextColor(Color.GREEN);
+            break;
+
+            case 40000:
+                moneyValue.setTextColor(Color.LTGRAY);
+                break;
+
+
+        }
         moneyValue.setText(String.valueOf(numberFormat.format(moneyCounter)));
+
+
             // Log.d("Main_Activity", "onCreate: Make It Rain! " + moneyCounter);
     }
 
