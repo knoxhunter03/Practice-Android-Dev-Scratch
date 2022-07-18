5cf035a2df6975fd2b4c52777171301da384a123
knoxhunter03 <mangalamkallo03@gmail.com>
2022-07-19 03:24:15 +0530

    CREATE & ADD SumOfDigits
    Note : Take care of Integer.parseInt() while converting EditText.getText() into number.
    
    It should be done inside a click method

---
+++ SumOfDigits/app/src/main/java/com/knoxhunter03/sumofdigits/MainActivity.java
@@ -0,0 +1,49 @@
+package com.knoxhunter03.sumofdigits;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText numberValue;
+    private Button addNumber;
+    private TextView sumValue;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        numberValue = findViewById(R.id.enterNumber);
+        sumValue = findViewById(R.id.sumValue);
+        addNumber = findViewById(R.id.buttonAdd);
+
+        addNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // ***Do type casting and all inside the click method!****
+                int number = Integer.parseInt(numberValue.getText().toString());
+                int sum = 0, rem;
+                while(number > 0){
+                    rem = number % 10;
+                    sum = sum + rem;
+                    number = number / 10;
+                }
+
+                // Log.d("Num", "onClick: " + number);
+                sumValue.setText("Sum : " + sum);
+            }
+        });
+
+    }
+
+
+}
