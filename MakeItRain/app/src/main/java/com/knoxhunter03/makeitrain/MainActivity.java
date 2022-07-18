e8431ebeb101fd2a49776f3c43d92cb210c1b54e
knoxhunter03 <mangalamkallo03@gmail.com>
2022-07-19 01:46:06 +0530

    ADD showMoney method to Button

--- MakeItRain/app/src/main/java/com/knoxhunter03/makeitrain/MainActivity.java
+++ MakeItRain/app/src/main/java/com/knoxhunter03/makeitrain/MainActivity.java
@@ -2,13 +2,37 @@ package com.knoxhunter03.makeitrain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.NumberFormat;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button makeItRain;
+    private Button showInfo;
+    private TextView moneyValue;
+
+    int moneyCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        makeItRain = findViewById(R.id.buttonMakeItRain);
+        showInfo = findViewById(R.id.buttonShowInfo);
+        moneyValue = findViewById(R.id.moneyValue);
+        // makeItRain.setOnClickListener(view -> Log.d("Main_Activity", "onClick: Make It Rain!"));
+
+
+    }
+
+    public void showMoney(View view) {
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+        moneyCounter += 1000;
+        moneyValue.setText(String.valueOf(numberFormat.format(moneyCounter)));
+            // Log.d("Main_Activity", "onCreate: Make It Rain! " + moneyCounter);
     }
 }
