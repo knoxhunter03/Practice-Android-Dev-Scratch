717442b56b0808bbce49f6ec121b09c0528d052a
knoxhunter03 <mangalamkallo03@gmail.com>
2022-07-19 02:01:40 +0530

    ADD Snackbar to Show Info Button

--- MakeItRain/app/src/main/java/com/knoxhunter03/makeitrain/MainActivity.java
+++ MakeItRain/app/src/main/java/com/knoxhunter03/makeitrain/MainActivity.java
@@ -8,6 +8,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     private Button makeItRain;
@@ -34,5 +37,15 @@ public class MainActivity extends AppCompatActivity {
         moneyCounter += 1000;
         moneyValue.setText(String.valueOf(numberFormat.format(moneyCounter)));
             // Log.d("Main_Activity", "onCreate: Make It Rain! " + moneyCounter);
+    }
+
+    public void showInfo(View view){
+        // Toast.makeText(MainActivity.this, R.string.app_info, Toast.LENGTH_SHORT).show();
+
+        Snackbar.make(moneyValue, R.string.app_info, Snackbar.LENGTH_LONG)
+                .setAction("More", view1 -> {
+                    Log.d("Snack", "showInfo: SnackBar");
+                })
+                .show();
     }
 }
