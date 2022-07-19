8f9dcfd945cab7e281def9897d1ab595e284ea9f
knoxhunter03 <mangalamkallo03@gmail.com>
2022-07-19 18:39:40 +0530

    UPDATE the app with Data Binding

--- Bio/app/src/main/java/com/knoxhunter03/bio/MainActivity.java
+++ Bio/app/src/main/java/com/knoxhunter03/bio/MainActivity.java
@@ -1,6 +1,8 @@
 package com.knoxhunter03.bio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingComponent;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -9,24 +11,40 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.knoxhunter03.bio.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     private EditText enterHobbies;
     private TextView hobbies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+//
+//        enterHobbies = findViewById(R.id.enter_hobbies);
+//        hobbies = findViewById(R.id.hobbies_text);
 
-        enterHobbies = findViewById(R.id.enter_hobbies);
-        hobbies = findViewById(R.id.hobbies_text);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addHobbies(view);
+            }
+        });
 
     }
 
     public void addHobbies(View view) {
-        hobbies.setText(String.format("Hobbies : %s", enterHobbies.getText().toString().trim()));
-        hobbies.setVisibility(View.VISIBLE);
+
+        binding.hobbiesText.setText(String.format("Hobbies : %s", binding.enterHobbies.getText().toString().trim()));
+        binding.invalidateAll(); 
+        binding.hobbiesText.setVisibility(View.VISIBLE);
+
+//        hobbies.setText(String.format("Hobbies : %s", enterHobbies.getText().toString().trim()));
+//        hobbies.setVisibility(View.VISIBLE);
 
         // Hide keyboard
 
